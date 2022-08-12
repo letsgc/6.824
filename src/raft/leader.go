c1c94052b040fb7e86a7b747205f63d994f912a5
chenglong <chenglong.1999@bytedance.com>
2022-08-12 10:54:47 +0800

    as follower while leader left cluster

--- src/raft/leader.go
+++ src/raft/leader.go
@@ -2,11 +2,13 @@ package raft
 
 import (
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
 func (rf *Raft) sendHB() {
 	var wg sync.WaitGroup
+	var followerCnt int32
 
 	for idx, _ := range rf.peers {
 		if idx == rf.me {
@@ -16,14 +18,20 @@ func (rf *Raft) sendHB() {
 		go func(idx int) {
 			defer wg.Done()
 			var resp AppendEntriesResp
+			var ok bool
 			RunWithTimeOut(func() {
-				rf.sendAppendEntries(idx, &AppendEntriesReq{rf.term, rf.me}, &resp)
+				ok = rf.sendAppendEntries(idx, &AppendEntriesReq{rf.term, rf.me}, &resp)
 			}, 1000*time.Millisecond)
 			if resp.Illegal {
 				rf.asFollower()
 			}
+			if ok {
+				atomic.AddInt32(&followerCnt, 1)
+			}
 		}(idx)
 	}
-
 	wg.Wait()
+	if followerCnt <= int32(len(rf.peers)/2) {
+		rf.asFollower()
+	}
 }
